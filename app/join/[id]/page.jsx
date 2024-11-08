8339025750faa5e92b3f7bf2a3f5623f5873666f
roark3377 <roarkmelva39@gmail.com>
2024-11-08 10:22:51 +0600

    Update page.jsx

--- app/join/[id]/page.jsx
+++ app/join/[id]/page.jsx
@@ -3,7 +3,10 @@ import UserModel from "@/db/User.Model";
 import { redirect } from "next/navigation";
 
 export default async function page({ params }) {
+  if (params.id== "create-user"){
+    return}
   await connectDB();
+  
   let user = await UserModel.findOne({
     uniqueId: params.id,
   });
